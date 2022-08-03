5e56e8e3866fa547d21612622733bd7d3f775565
Adam Krpan <adam.krpan@outlook.com>
2022-08-02 21:48:40 -0700

    * Rearranged Dockerfile for better rebuild caching * Changed API server address based on dev or prod run

--- src/rest/constants.tsx
+++ src/rest/constants.tsx
@@ -1,2 +1,10 @@
-export const ServerAddress = "localhost:10000"
+export let ServerAddress = ""
 export const ServerProtocol = "http://"
+
+const env = process.env.NODE_ENV
+if (env == "development") {
+    ServerAddress = "localhost:10000"
+}
+else {
+    ServerAddress = "192.168.1.5:30000"
+}
