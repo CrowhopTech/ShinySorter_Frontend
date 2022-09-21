0cafcc798fddee577b33572fc421efb9023488f3
Adam Krpan <adam.krpan@outlook.com>
2022-09-20 20:13:10 -0700

    Adds a home link to the 'no more files' page

--- pages/tagging/nofiles.tsx
+++ pages/tagging/nofiles.tsx
@@ -1,9 +1,13 @@
-import { Heading, VStack } from "@chakra-ui/react"
+import { Heading, Link, VStack } from "@chakra-ui/react"
+import { useRouter } from "next/router"
 
 const NoFiles = () => {
+    const router = useRouter();
+
     return <VStack alignItems="center" margin="50">
         <Heading size="2xl">No more files</Heading>
         <Heading size="xl">Check back later after more files have been added to tag!</Heading>
+        <Link size="xl" color="blue.500" onClick={() => router.push("/")}>Go back home</Link>
     </VStack>
 }
 
